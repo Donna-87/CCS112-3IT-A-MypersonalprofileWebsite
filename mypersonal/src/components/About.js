2d481a30c282cd7dea631e4ef0b9c2e82f6c938e
Donna-87 <cabuyaodonnamae87@gmail.com>
2025-04-30 11:31:07 +0800

    all the components

---
+++ mypersonal/src/components/About.js
@@ -0,0 +1,13 @@
+function About() {
+    return (
+      <div>
+        <h2>About Me</h2>
+      <p>Hello! I'm Donna Mae N. Cabuyao, a passionate learner with a deep love for technology and innovation.</p>
+      <p>In my free time, I enjoy coding, gaming, and biking — activities that keep both my mind and body active. I'm always eager to explore new ideas, learn new skills, and take on exciting challenges!</p>
+      <p>Whether it's diving into a new programming language, discovering the latest tech trends, or enjoying an adventurous bike ride, I'm all about growth, creativity, and pushing boundaries.</p>
+
+      </div>
+    );
+  }
+ 
+  export default About;
---
+++ mypersonal/src/components/Contact.js
@@ -0,0 +1,17 @@
+function Contact() {
+    return (
+      <div>
+        <h2>Contact</h2>
+        <p>Email: cabuyaodonnamae87@gmail.com</p>
+        <p>Number: 09701389896</p>
+        <p>Address: Niugan City of cabuyao Laguna</p>
+
+        <p>
+          <a href="https://github.com/Donna-87" target="_blank" rel="noreferrer">GitHub</a> |{" "}
+
+        </p>
+      </div>
+    );
+  }
+ 
+  export default Contact;
