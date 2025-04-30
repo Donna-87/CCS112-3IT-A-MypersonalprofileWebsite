e8974f8417018dc095613f0eedd92054140db377
Donna-87 <cabuyaodonnamae87@gmail.com>
2025-04-30 11:35:56 +0800

    update home

--- mypersonal/src/components/Home.js
+++ mypersonal/src/components/Home.js
@@ -2,7 +2,7 @@ function Home() {
     return (
       <div className="text-center">
         <h1>Welcome to My Profile</h1>
-        <p className="lead">Hi! I'm Renelyn Concina, and this is my personal site built with React.</p>
+        <p className="lead">Hi! I'm Donna Mae N. Cabuyao, and this is my personal site built with React.</p>
       </div>
     );
   }
